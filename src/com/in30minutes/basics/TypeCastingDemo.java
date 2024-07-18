1d1058d3f1095d065cb99b2ca2afd5bc2350fab2
Raj <raj.jobinfo97@gmail.com>
2024-07-18 11:52:42 +0530

    basics java files

---
+++ src/com/in30minutes/basics/TypeCastingDemo.java
@@ -0,0 +1,13 @@
+package com.in30minutes.basics;
+
+public class TypeCastingDemo {
+    public static void main(String[] args) {
+
+        byte a = 10;
+        int a1 = a;
+        //implicity typecast(normal typecast)
+        int b = 23;
+        byte b1 = (byte) b;
+        //explicity typecasting(manual typecasting)
+    }
+}
---
+++ src/com/in30minutes/basics/VariablesDemo.java
@@ -0,0 +1,15 @@
+package com.in30minutes.basics;
+
+public class VariablesDemo {
+    public static void main(String[] args) {
+        // boolean, byte, short, int, char, float, long, double
+        boolean isStudy = true;
+        byte a = 23;
+        short b = 22;
+        int age = 25;
+        char let = 'm';
+        float gpa = 23.5f;
+        long dif = 456L;
+        double dse = 342.45;
+    }
+}
